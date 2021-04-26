feeab278d8b7030380a16e03ae64ae6b30e0874e
benoitD03 <bens.92@hotmail.fr>
2021-04-26 19:46:04 +0200

    css portfolio ok

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -4,18 +4,19 @@
             <hr>
             <h2>Portfolio</h2>
         </div> 
-        <div id="websites" class="justify-content-center my-5">
-            <div class="websites col-md-4 mb-4 p-3" v-for="(website, index) in websites" :key="index">
-                <div class="front">
-                    <img class=" mr-2" v-bind:src="getImgUrl(website.image)" v-bind:alt="website.image">
+        <div class="website justify-content-between my-5">
+            <div class="websites col-lg-4 col-md-6 col-12 mb-4" v-for="(website, index) in websites" :key="index">
+                <div class="front mb-4">
+                    <img v-bind:src="getImgUrl(website.image)" v-bind:alt="website.image">
                 </div>
-                <div class="back">
+                <div class="back mb-4">
                     <p>{{ website.description }}</p>
                     <a :href="website.github">
-                        <button class="btn btn-primary">Repository GitHub</button>
+                        <button class="btn mb-3">Repository GitHub</button>
                     </a>
+                    <br>
                     <a :href="website.accomodation" v-if="website.accomodation != null">
-                        <button class="btn btn-primary">Voir le site</button>
+                        <button class="btn">Voir le site</button>
                     </a>
                 </div>
             </div>
@@ -28,8 +29,8 @@ export default {
      data() {
         return {
             websites : [
-                {name: 'Portfolio', image: 'image_portfolio.png', description: 'Portfolio personel, réalisé avec Vue.js', github: 'https://github.com/benoitD03/portfolio', accomodation: null},
-                {name: "Réseaux social d'entreprise", image: 'image_groupomania.png', description: "Création d'un réseau social d'entreprise, avec différents types de droits (admin et simple utilisateur), gestion des messages et des utilisateurs. Frontend réalisé avec Vue.js, et Backend avec NodeJS, MySQL et Sequelize.", github: 'https://github.com/benoitD03/BenoitDurand_7_020321', accomodation: null},
+                {name: 'Portfolio', image: 'image_portfolio.png', description: 'Portfolio personel, réalisé avec Vue.js. Vous êtes actuellement sur ce site ! ', github: 'https://github.com/benoitD03/portfolio', accomodation: null},
+                {name: "Réseaux social d'entreprise", image: 'image_groupomania.png', description: "Création d'un réseau social d'entreprise, avec gestion des messages et des utilisateurs. Frontend réalisé avec Vue.js, et Backend avec NodeJS, MySQL et Sequelize.", github: 'https://github.com/benoitD03/BenoitDurand_7_020321', accomodation: null},
                 {name: 'Répertoire pays', image: 'image_countries.png', description: "Intégration d'une API regroupant tous les pays du monde, ansi que quelques informations sur chaques pays. Réalisé avec Vue.js", github: "https://github.com/benoitD03/api-countries", accomodation: null},
                 {name: "API sécurisée pour une application d'avis gastronomique", image: 'image_sauces.png', description: "Création du Backend d'une application d'avis gastronomique, avec NodeJS, Express et MongoDB", github: "https://github.com/benoitD03/BenoitDurand_6_090221", accomodation: null},
                 {name: 'Intégration de maquette responsive', image: 'image_reservia.png', description: "Intégration d'une maquette responsive, uniquement en HTML et CSS", github: "https://github.com/benoitD03/BenoitDurand_2_161020", accomodation: "https://benoitd03.github.io/BenoitDurand_2_161020/"},
@@ -53,22 +54,47 @@ hr {
     width: 100px;
 }
 img {
-    width: 300px;
-    height: 170px;
+    width: 100%;
+    height: 200px;
+    border:8px solid #123b7a;
+    border-radius: 10px;
+    object-fit: cover;
 }
-#websites {
+.website {
     display: flex;
     flex-wrap: wrap;
     cursor: pointer;
+    position: relative;
 }
 .back {
-    opacity: 0;
+    text-align: center;
+    position: absolute;
+    top: 10%;
+    transform: scale(0);
+    transition: transform .5s ease-in-out;
 }
-.websites:hover .front {
-    opacity: 0;
-}
-.websites:hover .back {
-    opacity: 1;
+.front {
+    transform: translateY(0) rotateX(0);
+    transition: transform .5s ease-in-out;
 }
 
+.websites:hover .front {
+    transform: translateY(-150px) rotateX(-90deg);
+    
+}
+.websites:hover .back {
+    transform: scale(1);
+}
+button {
+    padding: 10px 20px;
+    border: 1px solid #e8ecf3;
+    border-radius: 8px;
+    background-color: #123b7a ;
+    color: #e8ecf3;
+    transition: background-color .2s ease-in;
+}
+button:hover {
+    color: #e8ecf3;
+    background-color: #a1b3cf;
+}
 </style>
