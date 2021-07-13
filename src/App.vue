295e6ce7558da6e59fe5206448296589c0795fdd
benoitD03 <bens.92@hotmail.fr>
2021-07-13 10:45:13 +0200

    add vue-smooth-scroll

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
         <nav class="navbar navbar-expand-lg fixed-top" style="background-color: #123b7a ">
             <div id="nav" class="collapse navbar-collapse justify-content-center">
                 <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link font-weight-bold text-uppercase text-light px-3" href="#">Présentation</a></li>
-                    <li class="nav-item"><a class="nav-link font-weight-bold text-uppercase text-light px-3" href="#skills">Compétences</a></li>
-                    <li class="nav-item"><a class="nav-link font-weight-bold text-uppercase text-light px-3" href="#experience">Expérience</a></li>
-                    <li class="nav-item"><a class="nav-link font-weight-bold text-uppercase text-light px-3" href="#portfolio">Portfolio</a></li>
-                    <li class="nav-item"><a class="nav-link font-weight-bold text-uppercase text-light px-3" href="#recommandations">Recommandations</a></li>
-                    <li class="nav-item"><a class="nav-link font-weight-bold text-uppercase text-light px-3" href="#contact">Contact</a></li>
+                    <li class="nav-item"><a class="nav-link font-weight-bold text-uppercase text-light px-3" href="#skills" v-smooth-scroll="{offset: -700}">Présentation</a></li>
+                    <li class="nav-item"><a class="nav-link font-weight-bold text-uppercase text-light px-3" href="#skills" v-smooth-scroll="{offset: -50}">Compétences</a></li>
+                    <li class="nav-item"><a class="nav-link font-weight-bold text-uppercase text-light px-3" href="#experience" v-smooth-scroll="{offset: -50}">Expérience</a></li>
+                    <li class="nav-item"><a class="nav-link font-weight-bold text-uppercase text-light px-3" href="#portfolio" v-smooth-scroll="{offset: -50}">Portfolio</a></li>
+                    <li class="nav-item"><a class="nav-link font-weight-bold text-uppercase text-light px-3" href="#recommandations" v-smooth-scroll="{offset: -50}">Recommandations</a></li>
+                    <li class="nav-item"><a class="nav-link font-weight-bold text-uppercase text-light px-3" href="#contact" v-smooth-scroll="{offset: -50}">Contact</a></li>
                 </ul>
             </div>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav"><span class="text-light"><i class="fas fa-bars fa-2x"></i></span></button>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import Vuelidate from 'vuelidate'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import vueSmoothScroll from 'vue2-smooth-scroll'
 
 Vue.use(Vuelidate)
+
+Vue.use(vueSmoothScroll)
 
 Vue.config.productionTip = false
 
