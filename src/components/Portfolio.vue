7d41e02d77a382f284e7cf782dc105ccba155505
benoitD03 <bens.92@hotmail.fr>
2021-09-09 16:15:28 +0200

    ajout projet vuex

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,100 +1,209 @@
 <template>
-    <div class="container">
-        <div class="text-center">
-            <hr>
-            <h2>Portfolio</h2>
-        </div> 
-        <div class="website justify-content-between my-5">
-            <div class="websites col-lg-4 col-md-6 col-12 mb-4" v-for="(website, index) in websites" :key="index">
-                <div class="front mb-4">
-                    <img v-bind:src="getImgUrl(website.image)" v-bind:alt="website.image">
-                </div>
-                <div class="back mb-4">
-                    <p>{{ website.description }}</p>
-                    <a :href="website.github" target="_blank">
-                        <button class="btn mb-3">Repository GitHub</button>
-                    </a>
-                    <br>
-                    <a :href="website.accomodation" target="_blank" v-if="website.accomodation != null">
-                        <button class="btn">Voir le site</button>
-                    </a>
-                </div>
-            </div>
-        </div>
+  <div class="container">
+    <div class="text-center">
+      <hr />
+      <h2>Portfolio</h2>
     </div>
+    <div class="website justify-content-between my-5">
+      <div
+        class="websites col-lg-4 col-md-6 col-12 mb-4"
+        v-for="(website, index) in websites"
+        :key="index"
+      >
+        <div class="front mb-4">
+          <img
+            v-bind:src="getImgUrl(website.image)"
+            v-bind:alt="website.image"
+          />
+        </div>
+        <div class="back mb-4">
+          <p>{{ website.description }}</p>
+          <a :href="website.github" target="_blank">
+            <button class="btn mb-3">Repository GitHub</button>
+          </a>
+          <br />
+          <a
+            :href="website.accomodation"
+            target="_blank"
+            v-if="website.accomodation != null"
+          >
+            <button class="btn">Voir le site</button>
+          </a>
+
+          <!-- Button trigger modal -->
+          <button
+            v-if="website.video"
+            type="button"
+            class="btn"
+            data-toggle="modal"
+            data-target="#exampleModal"
+          >
+            Démonstration
+          </button>
+        </div>
+      </div>
+    </div>
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">{{ websites[2].name }}</h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+              <p class="my-3">{{ websites[2].description }} Utilisation du localStorage pour le panier.</p>
+              <video controls>
+                  <source src="../assets/Video_Vuex.mp4" type="video/mp4"> 
+              </video>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-     data() {
-        return {
-            websites : [
-                {name: 'Portfolio', image: 'image_portfolio.png', description: 'Portfolio personel, réalisé avec Vue.js. Vous êtes actuellement sur ce site.', github: 'https://github.com/benoitD03/portfolio', accomodation: null},
-                {name: "Réseaux social d'entreprise", image: 'image_groupomania.png', description: "Création d'un réseau social d'entreprise, avec gestion des messages et des utilisateurs. Frontend réalisé avec Vue.js, et Backend avec NodeJS, MySQL et Sequelize.", github: 'https://github.com/benoitD03/BenoitDurand_7_020321', accomodation: null},
-                {name: 'Répertoire pays', image: 'image_countries.png', description: "Intégration d'une API regroupant tous les pays du monde, ansi que quelques informations sur chacun d'entre eux. Réalisé avec Vue.js", github: "https://github.com/benoitD03/api-countries", accomodation: null},
-                {name: "API sécurisée pour une application d'avis gastronomique", image: 'image_sauces.png', description: "Création du Backend d'une application d'avis gastronomique, avec NodeJS, Express et MongoDB", github: "https://github.com/benoitD03/BenoitDurand_6_090221", accomodation: null},
-                {name: 'Intégration de maquette responsive', image: 'image_reservia.png', description: "Intégration d'une maquette responsive, uniquement en HTML et CSS", github: "https://github.com/benoitD03/BenoitDurand_2_161020", accomodation: "https://benoitd03.github.io/BenoitDurand_2_161020/"},
-                {name: "Générateur de dégradé CSS", image: 'image_gradient_generator.png', description: "Générateur de dégradé CSS, réalisé en Javascript.", github: 'https://github.com/benoitD03/Gradient_Generator', accomodation: "https://benoitd03.github.io/Gradient_Generator"}
-            ]
-        }
+  data() {
+    return {
+      websites: [
+        {
+          name: "Portfolio",
+          image: "image_portfolio.png",
+          description:
+            "Portfolio personel, réalisé avec Vue.js. Vous êtes actuellement sur ce site.",
+          github: "https://github.com/benoitD03/portfolio",
+          accomodation: null,
+        },
+        {
+          name: "Réseaux social d'entreprise",
+          image: "image_groupomania.png",
+          description:
+            "Création d'un réseau social d'entreprise, avec gestion des messages et des utilisateurs. Frontend réalisé avec Vue.js, et Backend avec NodeJS, MySQL et Sequelize.",
+          github: "https://github.com/benoitD03/BenoitDurand_7_020321",
+          accomodation: null,
+        },
+        {
+          name: "E-commerce + interface administrateur",
+          image: "image_ecommerce.png",
+          description:
+            "E-commerce avec une interface administrateur. Simulation d'un serveur backend avec json-server, et frontend réalisé avec Vue.js / VUEX.",
+          github: "https://github.com/benoitD03/training_vuex_ecommerce",
+          accomodation: null,
+          video: "Video_Vuex.mp4",
+        },
+        {
+          name: "API sécurisée pour une application d'avis gastronomique",
+          image: "image_sauces.png",
+          description:
+            "Création du Backend d'une application d'avis gastronomique, avec NodeJS, Express et MongoDB",
+          github: "https://github.com/benoitD03/BenoitDurand_6_090221",
+          accomodation: null,
+        },
+        {
+          name: "Intégration de maquette responsive",
+          image: "image_reservia.png",
+          description:
+            "Intégration d'une maquette responsive, uniquement en HTML et CSS",
+          github: "https://github.com/benoitD03/BenoitDurand_2_161020",
+          accomodation: "https://benoitd03.github.io/BenoitDurand_2_161020/",
+        },
+        {
+          name: "Générateur de dégradé CSS",
+          image: "image_gradient_generator.png",
+          description: "Générateur de dégradé CSS, réalisé en Javascript.",
+          github: "https://github.com/benoitD03/Gradient_Generator",
+          accomodation: "https://benoitd03.github.io/Gradient_Generator",
+        },
+      ],
+    };
+  },
+  methods: {
+    getImgUrl(img) {
+      return require("../assets/" + img);
     },
-    methods: {
-        getImgUrl(img) {
-            return require('../assets/' + img);
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
 hr {
-    margin-top: 20px;
-    margin-bottom: 10px;
-    border-top: 2px solid #123b7a;
-    width: 100px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  border-top: 2px solid #123b7a;
+  width: 100px;
 }
 img {
-    width: 100%;
-    height: 200px;
-    border:8px solid #123b7a;
-    border-radius: 10px;
-    object-fit: cover;
+  width: 100%;
+  height: 200px;
+  border: 8px solid #123b7a;
+  border-radius: 10px;
+  object-fit: cover;
 }
 .website {
-    display: flex;
-    flex-wrap: wrap;
-    cursor: pointer;
-    position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  cursor: pointer;
+  position: relative;
 }
 .back {
-    text-align: center;
-    position: absolute;
-    top: 10%;
-    transform: translateY(150px) scale(0);
-    transition: transform .5s ease-in-out;
+  text-align: center;
+  position: absolute;
+  top: 10%;
+  transform: translateY(150px) scale(0);
+  transition: transform 0.5s ease-in-out;
 }
 .front {
-    transform: translateY(0) rotateX(0);
-    transition: transform .5s ease-in-out;
+  transform: translateY(0) rotateX(0);
+  transition: transform 0.5s ease-in-out;
 }
 
 .websites:hover .front {
-    transform: translateY(-150px) rotateX(-90deg);
-    
+  transform: translateY(-150px) rotateX(-90deg);
 }
 .websites:hover .back {
-    transform: translateY(0) scale(1);
+  transform: translateY(0) scale(1);
 }
 button {
-    padding: 10px 20px;
-    border: 1px solid #e8ecf3;
-    border-radius: 8px;
-    background-color: #123b7a ;
-    color: #e8ecf3;
-    transition: background-color .2s ease-in;
+  padding: 10px 20px;
+  border: 1px solid #e8ecf3;
+  border-radius: 8px;
+  background-color: #123b7a;
+  color: #e8ecf3;
+  transition: background-color 0.2s ease-in;
 }
 button:hover {
+  color: #e8ecf3;
+  background-color: #a1b3cf;
+}
+video {
+    width: 100%;
+}
+.modal-header {
     color: #e8ecf3;
-    background-color: #a1b3cf;
+    background: #123b7a;
 }
 </style>
--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -1,107 +1,115 @@
 <template>
-    <div id="about" class="container-fluid">
-        <div class="row mt-3">
-            <div id="photo" class="col-12 col-lg-4 my-5 text-center">
-                <img src="../assets/benoit.jpeg" alt="Benoit">  
-            </div>
-            <div class="my-auto col-12 col-lg-4 text-center heading">
-                <h1 class="text-light text-uppercase">Benoit Durand</h1>
-                <h2>Développeur Web</h2>
-                <a :href="`${publicPath}CV_Benoit.pdf`" class="button1" target="_blank">
-                    <button type="button" class="mt-3"><i class="fas fa-download"></i> Télécharger CV</button>
-                </a>
-                <div class="mt-3">
-                     <a class="network mr-4" href="https://github.com/benoitD03" target="_blank">
-                        <i class="fab fa-github fa-3x"></i>
-                    </a>
-                    <a class="network" href="https://www.linkedin.com/in/benoit-durand-556b94209/" target="_blank">
-                        <i class="fab fa-linkedin fa-3x"></i>
-                     </a>
-                </div> 
-            </div>  
+  <div id="about" class="container-fluid">
+    <div class="row mt-3">
+      <div id="photo" class="col-12 col-lg-4 my-5 text-center">
+        <img src="../assets/benoit.jpeg" alt="Benoit" />
+      </div>
+      <div class="my-auto col-12 col-lg-4 text-center heading">
+        <h1 class="text-light text-uppercase">Benoit Durand</h1>
+        <h2>Développeur Web</h2>
+        <a :href="`${publicPath}CV_Benoit.pdf`" class="button1" target="_blank">
+          <button type="button" class="mt-3">
+            <i class="fas fa-download"></i> Télécharger CV
+          </button>
+        </a>
+        <div class="mt-3">
+          <a
+            class="network mr-4"
+            href="https://github.com/benoitD03"
+            target="_blank"
+          >
+            <i class="fab fa-github fa-3x"></i>
+          </a>
+          <a
+            class="network"
+            href="https://www.linkedin.com/in/benoit-durand-556b94209/"
+            target="_blank"
+          >
+            <i class="fab fa-linkedin fa-3x"></i>
+          </a>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 
 <script>
 export default {
-    data() {
-        return {
-            publicPath: process.env.BASE_URL
-        }
-    }
-}
+  data() {
+    return {
+      publicPath: process.env.BASE_URL,
+    };
+  },
+};
 </script>
 
 <style scoped>
 .row {
-    justify-content: center ;
+  justify-content: center;
 }
 #about {
-    padding: 75px 300px 25px 300px;
-    background: #a1b3cf;
-    flex-wrap: wrap; 
+  padding: 75px 300px 25px 300px;
+  background: #a1b3cf;
+  flex-wrap: wrap;
 }
 img {
-    width: 250px;
-    border-radius: 50%;
-    border:8px solid #123b7a;
-     box-shadow: 0 0 0 2px #e8ecf3; 
+  width: 250px;
+  border-radius: 50%;
+  border: 8px solid #123b7a;
+  box-shadow: 0 0 0 2px #e8ecf3;
 }
 button {
-    padding: 10px 20px;
-    border: 1px solid #e8ecf3;
-    border-radius: 8px;
-    background-color: #123b7a ;
-    color: #e8ecf3;
-    transform: scale(1);
-    transition: transform .3s ease-in-out;
+  padding: 10px 20px;
+  border: 1px solid #e8ecf3;
+  border-radius: 8px;
+  background-color: #123b7a;
+  color: #e8ecf3;
+  transform: scale(1);
+  transition: transform 0.3s ease-in-out;
 }
 .network {
-    color: #123b7a;   
+  color: #123b7a;
 }
 .network i {
-    transform: rotate(0);
-    transition: transform .5s ease-in-out;
+  transform: rotate(0);
+  transition: transform 0.5s ease-in-out;
 }
 .network i:hover {
-    transform: rotate(360deg);
+  transform: rotate(360deg);
 }
 button:hover {
-    transform: scale(1.20);
+  transform: scale(1.2);
 }
 #photo {
-    animation: scale-up-center .8s cubic-bezier(0.390, 0.575, 0.565, 1.000) both .3s;
+  animation: scale-up-center 0.8s cubic-bezier(0.39, 0.575, 0.565, 1) both 0.3s;
 }
 
 @keyframes scale-up-center {
   0% {
-            transform: scale(0);
+    transform: scale(0);
   }
   70% {
-            transform: scale(1.5);
+    transform: scale(1.5);
   }
   100% {
-            transform: scale(1);
+    transform: scale(1);
   }
 }
 @media all and (max-width: 1000px) {
-    img {
-        width: 175px;
-    }
-    h1 {
-        font-size: 30px;
-    }
-    h2 {
-        font-size: 25px;
-    }
+  img {
+    width: 175px;
+  }
+  h1 {
+    font-size: 30px;
+  }
+  h2 {
+    font-size: 25px;
+  }
 }
-@media all and  (max-width: 1500px){
-    #about {
-        padding: 60px;
-    }
+@media all and (max-width: 1500px) {
+  #about {
+    padding: 60px;
+  }
 }
-
-
 </style>
